4c7f8b612d31bcf5a329429eaed71c2ee4f44baf
Rayson-Joanna <131461485+Rayson-Joanna@users.noreply.github.com>
2023-04-21 22:20:11 +0900

    Add files via upload

---
+++ mingky/src/ass/yummy.java
@@ -0,0 +1,17 @@
+package ass;
+
+public class yummy {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 3;
+		String b;
+		b = "¹Ö¶Ë¹Ù";
+		
+		int c = 4;
+		
+		System.out.println(b+c);
+		System.out.println(a+b);
+	}
+
+}
